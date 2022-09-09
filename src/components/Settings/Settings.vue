4ecc52362c848d4607aaa1ecc0d88ca8396f9739
Ivanov Alexander <ivanovaa1992@gmail.com>
2022-09-09 23:35:09 +0300

    start settings

---
+++ src/components/Settings/Settings.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="settings">
+    <span class="settings__title">Settings</span>
+    <button class="settings__close"></button>
+    <div class="settings__cities"></div>
+    <div class="settings__add-new"></div>
+  </div>
+</template>
+<script>
+import { fetchWeather } from "@/api";
+
+export default {
+  name: "Settings",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "./Settings.scss";
+</style>
